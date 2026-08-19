fda29b598c2f078699ae13eaaf1e5bdd26b301de
Oseias-Augusto <oseiasafpm@gmail.com>
2026-08-19 20:40:11 -0300

    Commit de background simplified

--- main/src/components/inicio/Hero.jsx
+++ main/src/components/inicio/Hero.jsx
@@ -1,11 +1,10 @@
-import carro from "../../assets/carro.png"
-import 
+ 
 
 export default function Hero({ telaInicial ,setTelaInicial }) {
 
     return (
-        <div>
-           <img src={carro} alt="Fiat Uno"/>
-        </div>
+        <>
+            
+        </>
     )
 }
--- main/src/components/ui/background/Background.jsx
+++ main/src/components/ui/background/Background.jsx
@@ -1,4 +1,17 @@
+import carro from "../../../assets/carro.png";
+import styles from "./Background.module.css";
 
-export default function Background({  }) {
+
+export default function Background() {
+
+    return (
+        <>
+            <div className={styles.fundo} >
+                <div className={styles.contImage}>
+                    <img className={styles.image} src={carro} alt="Fiat Mobi" />
+                </div>
+            </div>
+        </>
+    )
 
 }
